7e44a48d99fc29c34ede024ca3b925e2f95ef630
Huehtt1202@ <Hoangtthue0220@gmail.com>
2022-11-29 17:07:08 +0700

    Change repository

---
+++ EntityFramework/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        void Insert();
+        void Update(T Entity);
+        void Delete(T Entity);
+        T FindById(string id);
+        void Save(T Entity);
+    }
+}
---
+++ EntityFramework/Repository/Repository.cs
@@ -0,0 +1,46 @@
+﻿using EntityFramework.Connection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly crmDbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+        public Repository(crmDbContext dbContext, DbSet<T> dbSet)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbSet;
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.AsEnumerable();
+        }
+        public T FindById(string id)
+        {
+            return _dbSet.FirstOrDefault();
+        }
+        public void Delete(T Entity)
+        { 
+            _dbSet.Remove(Entity);
+        }
+        public void Insert(T Entity)
+        {
+            _dbSet.Add(Entity);
+        }
+        public void Update(T Entity)
+        {
+            _dbSet.Update(Entity);
+        }
+        public void Save(T Entity)
+        {
+            _dbSet.SaveChange()
+        }
+    }
+}
