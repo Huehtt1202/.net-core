7bb4168f532aef7061e38b1392fac058d9ffc7ea
LAPTOP-J36DFC9Q\ThuHue <hoangtthue0220@gmail.com>
2022-12-06 21:56:40 +0700

    Change new

--- Service/DomainService/UserDomainService.cs
+++ Service/DomainService/UserDomainService.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entity;
+using DocumentFormat.OpenXml.Wordprocessing;
 using EntityFramework.Connection;
 using EntityFramework.Repository;
 using Microsoft.EntityFrameworkCore;
@@ -6,6 +7,7 @@ using Service.IDomainService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,34 +15,6 @@ namespace Service.DomainService
 {
     public class UserDomainService : Repository<User>, IUserDomainService
     {
-        private readonly crmDbContext _dbContext;
-        private readonly DbSet<User> _user;
-        private readonly Repository<User> _repository;
-        public UserDomainService(crmDbContext dbContext, User user, Repository<User> repository)
-        {
-            _dbContext = dbContext;
-            _user = user;
-            _repository = repository;
-        }
 
-        public void Login(string username, string password)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Logout()
-        {
-            throw new NotImplementedException();
-        }
-
-        public string MD5(string password)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string ResetPassword(string password)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
